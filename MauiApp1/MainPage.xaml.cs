3bb84337a5663aa75d17a443b12a5bbd56209270
song-owner <si@oosaki.local>
2022-06-07 13:33:47 +0900

    startMac

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+    private void startSearch(object sender, EventArgs e)
+    {
+        var abc = new ResultPage(seartchWord.Text);
+        //Navigation.PushAsync(abc,true);
+        Application.Current.MainPage = abc;
+        //Navigation.PushAsync(new ResultPage(seartchWord.Text));
+    }
+}
+
---
+++ MauiApp1/ResultPageTap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public partial class ResultPage
+    {
+        private void appTapped(object sender, EventArgs e)
+        {
+            if (slider.Value == 0 || slider.Value == 1) googlePageOpen(); else appPageOpen();
+        }
+        private void googleTapped(object sender, EventArgs e)
+        {
+            if (slider.Value == 0) appPageOpen(); else googlePageOpen();
+        }
+        void googlePageOpen()
+        {
+            GooglePage.Width = GridLength.Star;
+            AppPge.Width = 0;
+            slider.Value = AppPge.Width.Value / resultPage.Width;
+        }
+        void appPageOpen()
+        {
+            AppPge.Width = GridLength.Star;
+            GooglePage.Width = 0;
+            slider.Value = (resultPage.Width - GooglePage.Width.Value) / resultPage.Width;
+        }
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var width = resultPage.Width;
+            sliderValue = e.NewValue;
+            AppPge.Width = width * sliderValue;
+            GooglePage.Width = GridLength.Star;
+        }
+    }
+}
